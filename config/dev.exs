9f47ed09be7ffda85f729f3ab17748bf7f1bcaad
pCosta99 <costapedro.a1999@gmail.com>
2021-10-17 02:13:36 +0100

    tailwind + alpinejs basic setup

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :bundle, BundleWeb.Endpoint,
   secret_key_base: "y//lHuFugTIrHpx95mY3UyWYii3aWJ06sHz8veli8ZuFQ25n2QfNWk4/38uxfgdd",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
