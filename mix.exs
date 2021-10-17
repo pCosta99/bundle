93e45ec8018105a947f98943338df3dc8c3a4889
pCosta99 <costapedro.a1999@gmail.com>
2021-10-17 02:21:56 +0100

    fixing ci, sobelow on test

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Bundle.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:sobelow, "~> 0.8", only: :dev}
+      {:sobelow, "~> 0.8", only: [:dev, :test]}
     ]
   end
 
